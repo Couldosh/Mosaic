8f937c906e0eec6b265fe089e7dbce818c5337ba
Fabrice Simon <fabrice.kergaline.simon@gmail.com>
2020-07-23 05:11:46 +0200

    Add the dominant color

--- src/Pixelisator.java
+++ src/Pixelisator.java
@@ -1,24 +1,58 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.Raster;
-import java.awt.image.WritableRaster;
+import java.util.*;
+import java.util.List;
 
-public class Pixelisator {
-    public static WritableRaster pixelise(BufferedImage image,int pixelisation){
-        Raster rasterSrc = image.getData();
-        WritableRaster dest = rasterSrc.createCompatibleWritableRaster();
+public final class Pixelisator {
 
-        for(int y = 0; y < rasterSrc.getHeight(); y += pixelisation) {
-            for(int x = 0; x < rasterSrc.getWidth(); x += pixelisation) {
-                double[] pixel = new double[3];
-                pixel = rasterSrc.getPixel(x, y, pixel);
+    public static BufferedImage pixelate(BufferedImage imageToPixelate, int pixelSize) {
+        BufferedImage pixelateImage = new BufferedImage(
+                imageToPixelate.getWidth(),
+                imageToPixelate.getHeight(),
+                imageToPixelate.getType());
 
-                for(int yd = y; (yd < y + pixelisation) && (yd < dest.getHeight()); yd++) {
-                    for(int xd = x; (xd < x + pixelisation) && (xd < dest.getWidth()); xd++) {
-                        dest.setPixel(xd, yd, pixel);
+        for (int y = 0; y < imageToPixelate.getHeight(); y += pixelSize) {
+            for (int x = 0; x < imageToPixelate.getWidth(); x += pixelSize) {
+                BufferedImage croppedImage = getCroppedImage(imageToPixelate, x, y, pixelSize, pixelSize);
+                Color dominantColor = getDominantColor(croppedImage);
+                for (int yd = y; (yd < y + pixelSize) && (yd < pixelateImage.getHeight()); yd++) {
+                    for (int xd = x; (xd < x + pixelSize) && (xd < pixelateImage.getWidth()); xd++) {
+                        pixelateImage.setRGB(xd, yd, dominantColor.getRGB());
                     }
                 }
             }
         }
-        return dest;
+
+        return pixelateImage;
+    }
+
+    public static BufferedImage getCroppedImage(BufferedImage image, int startx, int starty, int width, int height) {
+        if (startx < 0) startx = 0;
+        if (starty < 0) starty = 0;
+        if (startx > image.getWidth()) startx = image.getWidth();
+        if (starty > image.getHeight()) starty = image.getHeight();
+        if (startx + width > image.getWidth()) width = image.getWidth() - startx;
+        if (starty + height > image.getHeight()) height = image.getHeight() - starty;
+        return image.getSubimage(startx, starty, width, height);
+    }
+
+    public static Color getDominantColor(BufferedImage image) {
+        Map<Integer, Integer> colorCounter = new HashMap<>(100);
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                int currentRGB = image.getRGB(x, y);
+                int count = colorCounter.getOrDefault(currentRGB, 0);
+                colorCounter.put(currentRGB, count + 1);
+            }
+        }
+        return getDominantColor(colorCounter);
+    }
+
+    private static Color getDominantColor(Map<Integer, Integer> colorCounter) {
+        int dominantRGB = colorCounter.entrySet().stream()
+                .max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1)
+                .get()
+                .getKey();
+        return new Color(dominantRGB);
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 
 public class Runner {
     public static void main(String[] args) {
-        int pixelisation = 5;
+        int pixelisation = 10;
         try {
             BufferedImage imageSrc = ImageIO.read(new File("./images/img1.jpg"));
 
-            imageSrc.setData(Pixelisator.pixelise(imageSrc,pixelisation));
-            ImageIO.write(imageSrc, "jpg", new File("./images/image-pixelated.jpg"));
+            BufferedImage dest = Pixelisator.pixelate(imageSrc,pixelisation);
+            ImageIO.write(dest, "jpg", new File("./images/image-pixelated.jpg"));
         } catch (IOException e) {
             System.out.println("RIP");
         }
